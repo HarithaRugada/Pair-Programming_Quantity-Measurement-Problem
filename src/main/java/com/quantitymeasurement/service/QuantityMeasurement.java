6c44152e970ddedaa55a48ccbf02ffe5933a09b7
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-16 16:28:36 +0530

    ADDED: Code  for equating the feet with feet

--- src/main/java/com/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/com/quantitymeasurement/service/QuantityMeasurement.java
@@ -1,4 +1,30 @@
 package com.quantitymeasurement.service;
 
+import java.util.Objects;
+
 public class QuantityMeasurement {
-}
+        public double value;
+        public Unit unitType;
+
+        public QuantityMeasurement() {
+        }
+
+        //Units
+        public enum Unit {FEET, INCH, YARD}
+
+        public  QuantityMeasurement(Double value, Unit unitType) {
+            this.value = value;
+            this.unitType = unitType;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            QuantityMeasurement that = (QuantityMeasurement) o;
+            if (Objects.equals(value, that.value))
+                return true;
+            return false;
+        }
+
+
+    }
